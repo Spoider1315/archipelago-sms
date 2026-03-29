81456d941732654a1a155e60041711d74415ff15
Spoider1315 <spiderhawk1315@gmail.com>
2026-03-28 19:24:18 -0500

    Adding difficultly settings to Pianta Village shines. (Have not done blue coins or nozzles yet)

--- worlds/sms/sms_regions/pianta_village.py
+++ worlds/sms/sms_regions/pianta_village.py
@@ -6,10 +6,10 @@ PIANTA_VILLAGE_ENTRANCE: SmsRegion = SmsRegion(SmsRegionName.PIANTA_ENTRANCE,
     ticketed="Pianta Village Ticket", parent_region=SmsRegionName.PLAZA)
 
 PIANTA_VILLAGE_ANY: SmsRegion = SmsRegion(SmsRegionName.PIANTA_ANY,
-    shines=[Shine("Chain Chomplets Unchained", [Requirements(ANY_SPLASHER)], in_game_bit=60),
+    shines=[Shine("Chain Chomplets Unchained", requirements=[Requirements(SPRAY_OR_HOVER)], hard=[Requirements(SPRAY_OR_HOVER_OR_TURBO)], in_game_bit=60),
         Shine("Il Piantissimo's Crazy Climb", [Requirements(location=f"{SmsRegionName.PIANTA_ANY} - Chain Chomplets Unchained")], in_game_bit=65),  # Req. None
-        Shine("The Goopy Inferno", [Requirements([[NozzleType.hover]])], in_game_bit=62),
-        Shine("Chain Chomp's Bath", [Requirements(ANY_SPLASHER)], in_game_bit=61),
+        Shine("The Goopy Inferno", requirements=[Requirements(SPROCKET_OR_HOVER)], hard=[Requirements(SPROCKET_OR_HOVER_OR_TURBO)], tears=[Requirements(SPRAY_OR_HOVER_OR_TURBO)], in_game_bit=62),
+        Shine("Chain Chomp's Bath", requirements=[Requirements([[NozzleType.spray]])], hard=[Requirements(manual_none=True)], in_game_bit=61),
         Shine("100 Coins", [Requirements(ALL_SPLASHER)], hundred=True, in_game_bit=106)],
     blue_coins=[BlueCoin("River End", in_game_bit=432),
         BlueCoin("Grass", in_game_bit=433),
@@ -51,11 +51,11 @@ PIANTA_VILLAGE_FIVE_ONLY: SmsRegion = SmsRegion(SmsRegionName.PIANTA_FIVE_ONLY,
 
 PIANTA_VILLAGE_FIVE_BEYOND: SmsRegion = SmsRegion(SmsRegionName.PIANTA_FIVE_BEYOND,
     requirements=[Requirements([[NozzleType.yoshi]])],
-    shines=[Shine("Secret of the Village Underside", [Requirements([[NozzleType.yoshi]])], in_game_bit=64),
-        Shine("Piantas in Need", [Requirements(ANY_SPLASHER)], in_game_bit=63),
-        Shine("Shadow Mario Runs Wild", [Requirements([[NozzleType.spray]], location=f"{SmsRegionName.PIANTA_FIVE_BEYOND} - Piantas in Need")], in_game_bit=66),
-        Shine("Fluff Festival Coin Hunt", [Requirements(ROCKET_OR_HOVER, location=f"{SmsRegionName.PIANTA_FIVE_BEYOND} - Shadow Mario Runs Wild")], in_game_bit=67),
-        Shine("Red Coin Chucksters", [Requirements([[NozzleType.hover]])], in_game_bit=68)
+    shines=[Shine("Secret of the Village Underside", requirements=[Requirements([[NozzleType.yoshi]])], advanced=[Requirements(HOVER_OR_YOSHI)], in_game_bit=64),
+        Shine("Piantas in Need", requirements=[Requirements(SPRAY_OR_HOVER)], advanced=[Requirements(SPRAY_OR_HOVER_OR_TURBO)], in_game_bit=63),
+        Shine("Shadow Mario Runs Wild", requirements=[Requirements([[NozzleType.spray]], location=f"{SmsRegionName.PIANTA_FIVE_BEYOND} - Piantas in Need")], advanced=[Requirements(SPRAY_OR_HOVER, location=f"{SmsRegionName.PIANTA_FIVE_BEYOND} - Piantas in Need")], in_game_bit=66),
+        Shine("Fluff Festival Coin Hunt", requirements=[Requirements(ROCKET_OR_HOVER, location=f"{SmsRegionName.PIANTA_FIVE_BEYOND} - Shadow Mario Runs Wild")], advanced=[Requirements(ROCKET_OR_HOVER_OR_TURBO, location=f"{SmsRegionName.PIANTA_FIVE_BEYOND} - Shadow Mario Runs Wild")], in_game_bit=67),
+        Shine("Red Coin Chucksters", requirements=[Requirements(YOSHI_AND_HOVER)], advanced=[Requirements([[NozzleType.hover]])], tears=[Requirements(HOVER_OR_YOSHI)], in_game_bit=68)
     ], parent_region=SmsRegionName.PIANTA_ENTRANCE)
 
 PIANTA_VILLAGE_SIX: SmsRegion = SmsRegion(SmsRegionName.PIANTA_SIX,
@@ -72,7 +72,7 @@ PIANTA_VILLAGE_SIX: SmsRegion = SmsRegion(SmsRegionName.PIANTA_SIX,
 
 PIANTA_VILLAGE_EIGHT: SmsRegion = SmsRegion(SmsRegionName.PIANTA_EIGHT,
     requirements=[Requirements([[NozzleType.yoshi]], location=f"{SmsRegionName.PIANTA_FIVE_BEYOND} - Shadow Mario Runs Wild")],
-    shines=[Shine("Soak the Sun", [Requirements(SPRAY_AND_HOVER_OR_ROCKET)], in_game_bit=69)],
+    shines=[Shine("Soak the Sun", requirements=[Requirements(SPRAY_AND_ROCKET_OR_SPRAY_AND_HOVER)], hard=[Requirements(SPRAY_AND_OTHER_FLUDD)], in_game_bit=69)],
     blue_coins=[BlueCoin("Bird", [Requirements(SPRAY_AND_HOVER)], in_game_bit=441)],
     nozzle_boxes=[NozzleBox("Rocket Box", [Requirements([[NozzleType.hover]])], in_game_bit=882)],
     parent_region=SmsRegionName.PIANTA_ENTRANCE)
